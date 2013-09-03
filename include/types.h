596ad4310e2ff8af233a5369cc9606e925599097
southernbear <xq5530303@gmail.com>
2013-09-03 11:57:36 +0800

    fix type conflicting in types.h

--- include/types.h
+++ include/types.h
@@ -7,16 +7,12 @@
 #define TYPES_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef uint32_t ptr_t;
 typedef uintptr_t memptr_t;
 
 typedef uint32_t l4_thread_t;
-
-typedef uint32_t size_t;
-
-#define NULL \
-	((ptr_t) 0x0)
 
 #define ALIGNED(size, align) \
 	(size / align) + ((size & (align - 1)) != 0)
