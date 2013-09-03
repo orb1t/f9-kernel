e6a630b80deedc2fd574a71f16e52ee5c238d0e9
southernbear <xq5530303@gmail.com>
2013-09-03 10:12:43 +0800

    libl4: fix types.h by copying codes from included

--- user/lib/libl4/include/l4lib/arch/arm/types.h
+++ user/lib/libl4/include/l4lib/arch/arm/types.h
@@ -3,6 +3,16 @@
 
 #define TASK_ID_INVALID			0xFFFFFFFF
 
-#include <l4/arch/arm/types.h>
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+typedef signed long long s64;
+typedef signed int s32;
+typedef signed short s16;
+typedef signed char s8;
+
+/* Thread/Space id type */
+typedef unsigned int l4id_t;
 
 #endif /* __L4LIB_ARM_TYPES_H__ */
